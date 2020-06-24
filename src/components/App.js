f85d84205ea8d736685e3834866b1a2f9034158b
Beth-A <elizabeth78@me.com>
2020-06-24 12:01:57 -0400

    Convert css to scss

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 import Jumbotron from "./Jumbotron";
 import Card from "./Card";
 import projects from "../projects";
-import "../components/sass/style.scss";
+import "../components/sass/main.scss";
 
 function createCard(project) {
   return (
